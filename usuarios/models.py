faacf01f08a44f64c073f24a587f36af04a797a7
1zcan <orlandoaravenav568@gmail.com>
2025-11-19 17:35:33 -0300

    te he

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,18 +4,20 @@ from django.utils import timezone
 
 
 class Perfil(models.Model):
-
+   
     ROLES = (
-        ('administrador', 'Administrador'),
-        ('editor', 'Editor'),
-        ('usuario', 'Usuario'),
+        ('ti_informatica', 'TI / Informática'),     
+        ('profesional_salud', 'Profesional Salud'),  
+        ('tecnico_salud', 'Técnico Salud'),          
+        ('administrativo', 'Administrativo'),       
     )
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
-    rol = models.CharField(max_length=20, choices=ROLES, default='usuario')
+    rol = models.CharField(max_length=20, choices=ROLES, default='administrativo')
     foto = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.rol}"
+        return f"{self.user.username} - {self.get_rol_display()}"
 
 
 class TwoFactorCode(models.Model):
